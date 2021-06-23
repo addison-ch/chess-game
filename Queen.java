306f81046155920a24751d14e0eb2d495a2f86bd
Addison <74084822+addison-ch@users.noreply.github.com>
2021-06-23 03:32:56 -0400

    Add files via upload

---
+++ Queen.java
@@ -0,0 +1,44 @@
+public class Queen extends Piece {
+
+    public Queen(int x, int y, boolean isWhite, boolean isAlive) {
+        super(x, y, isWhite, isAlive);
+    }
+
+    @Override
+    public String toString() {
+        if (this.getIsWhite() == true) {
+            return "Q";
+        } else {
+            return "q";
+        }
+    }
+
+    @Override
+    public boolean possibleMove(int x, int y) {
+        if (Board.board[x][y] != null) {
+            if (Board.board[x][y].getIsWhite() == this.getIsWhite()) {
+                return false;
+            }
+        }
+
+        if (this.getX() == x && this.getY() == y) {
+            return false;
+        }
+
+        if (Math.abs(this.getX() - x) == Math.abs(this.getY() - y)
+                && Board.isNotBlocked(this.getX(), this.getY(), x, y)) {
+            return true;
+        }
+
+        if (Math.abs(this.getX() - x) != 0 && Math.abs(this.getY() - y) == 0
+                && Board.isNotBlocked(this.getX(), this.getY(), x, y)) {
+            return true;
+        }
+        if (Math.abs(this.getX() - x) == 0 && Math.abs(this.getY() - y) != 0
+                && Board.isNotBlocked(this.getX(), this.getY(), x, y)) {
+            return true;
+        }
+        return false;
+    }
+
+}
